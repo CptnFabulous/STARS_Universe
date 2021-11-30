49a72d25cef5078f2e0fa64ca1f83063e4c68f91
Marcus Mouyat <marcus@mouyat.com>
2021-11-30 13:57:05 +1100

    Static functions to convert sensitivity values to and from a slider range
    I created two functions, SensitivityToSliderValue() and SliderValueToSensitivity(). The first is a void function that takes a current sensitivity value, a max sensitivity value, and a Slider object, and updates the Slider component to display an equivalent value, but within the slider’s range. The SliderValueToSensitivity() function is a float that converts it back to a value usable by the game process. Sometimes a sensitivity value can be very large or small and look weird to the end user, so these functions exist to add a layer of abstraction and, to the player in the options menu, present a more sensible appearing number range to pick from.

--- Assets/Scripts/HUD/OptionsMenu.cs
+++ Assets/Scripts/HUD/OptionsMenu.cs
@@ -53,4 +53,27 @@ public abstract class OptionsMenu : MonoBehaviour
     public abstract void ApplySettings();
 
 
+    /// <summary>
+    /// Update a slider's value to represent a scaled sensitivity value. Some sensitivity values can be very large or small numbers, so this converts them to a slider range designed for better UX
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="absoluteMaxValue"></param>
+    /// <returns></returns>
+    public static float SliderValueToSensitivity(Slider value, float maxValue)
+    {
+        return value.value / value.maxValue * maxValue;
+    }
+
+    /// <summary>
+    /// Converts a slider value from the slider's range (optimised for UX) to a value of similar scale but usable for its intended purpose.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="currentValue"></param>
+    /// <param name="maxValue"></param>
+    public static void SensitivityToSliderValue(Slider value, float currentValue, float maxValue)
+    {
+        // Update the options to reflect the current value.
+        float valueToShow = currentValue / maxValue * value.maxValue; // Turns original value into a -1 to 1 range
+        value.value = Mathf.Clamp(valueToShow, value.minValue, value.maxValue); // Clamp value and update slider accordingly
+    }
 }
