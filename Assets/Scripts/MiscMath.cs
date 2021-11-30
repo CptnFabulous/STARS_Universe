2bf45f1824e7802966f15ba7a399480b4856475b
Marcus Mouyat <marcus@mouyat.com>
2021-11-30 13:22:44 +1100

    MiscMath class for generic functions I want to use across multiple classes
    This function presently includes a function for multiplying two Vector3s, and one for clamping a Vector3. I used these for my spaceship controller, but I might need them for other purposes as well, so I put them in this 'miscellaneous' class.

---
+++ Assets/Scripts/MiscMath.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MiscMath
+{
+    public static Vector3 Vector3Multiply(Vector3 lhs, Vector3 rhs)
+    {
+        lhs.x *= rhs.x;
+        lhs.y *= rhs.y;
+        lhs.z *= rhs.z;
+        return lhs;
+    }
+    public static Vector3 Vector3Clamp(Vector3 vector, Vector3 min, Vector3 max)
+    {
+        vector.x = Mathf.Clamp(vector.x, min.x, max.x);
+        vector.y = Mathf.Clamp(vector.y, min.y, max.y);
+        vector.z = Mathf.Clamp(vector.z, min.z, max.z);
+        return vector;
+    }
+}
