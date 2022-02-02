f7374622b6a0f997a79333f891a7a28a2d2f092c
Marcus Mouyat <marcus@mouyat.com>
2022-02-02 19:34:19 +1100

    Advanced spaceship navigation GUI
    I am starting work on a new mechanic that lets the user temporarily show a wide range of additional HUD information, the kind that is generally too obtrusive for normal gameplay. This code required many overhauls to different classes:
    
    SpaceshipNavigationGUI changes:
    Added a series of world space compass axes that appear around the sun using LineRenderer classes> This is the main advanced navigation feature that can be turned on and off.
    Added code to the SpaceshipNavigationGUI class to show and hide advanced navigation information. The mechanic can be enabled or disabled by tapping the compass on-screen, although there is presently no keyboard command for it.
    Added a readonly array of Vector3 values representing the six axis directions. Each set of variables in the class that corresponds to these directions is now an array, such as axis colours and compass points. I made this change so I can use for loops instead of running every axis-related piece of code six times to account for all of them.
    
    Player prefabs:
    Added new variables for these new code changes to the Player Spaceship prefab, as well as making the navigation button its own prefab variant for easier standalone editing.
    Changed some of the compass axis colours.
    
    SolarSystem class:
    This MonoBehaviour class attaches to the GameObject representing the sun, or whatever object is intended to exist at the centre of the level. This contains general information about the current level, that the SpaceshipNavigationGUI class references to display appropriate information.

---
+++ Assets/Scripts/Space environment/SolarSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystem : MonoBehaviour
+{
+    public float radius = 5000;
+    public Collider sun;
+    public float minSafeDistanceFromSun = 2000;
+
+    public static SolarSystem Current
+    {
+        get
+        {
+            if (currentInstance == null)
+            {
+                currentInstance = FindObjectOfType<SolarSystem>();
+            }
+            return currentInstance;
+        }
+    }
+    static SolarSystem currentInstance;
+}
--- Assets/Scripts/SpaceshipNavigationGUI.cs
+++ Assets/Scripts/SpaceshipNavigationGUI.cs
@@ -6,35 +6,101 @@ public class SpaceshipNavigationGUI : MonoBehaviour
 {
     public SpaceshipMovement controller;
 
-
-    [Header("Axis colours")]
-    public Color colourPlusX = Color.red;
-    public Color colourPlusY = Color.green;
-    public Color colourPlusZ = Color.blue;
-    public Color colourMinusX = Color.cyan;
-    public Color colourMinusY = Color.magenta;
-    public Color colourMinusZ = Color.yellow;
-
     [Header("Compass")]
-    public Transform compassTransform;
-    public Renderer compassPlusX;
-    public Renderer compassPlusY;
-    public Renderer compassPlusZ;
-    public Renderer compassMinusX;
-    public Renderer compassMinusY;
-    public Renderer compassMinusZ;
-    
-    void Start()
+    public Color[] axisColours = new Color[]
     {
-        // Sets compass colours
-        compassPlusX.material.color = colourPlusX;
-        compassPlusY.material.color = colourPlusY;
-        compassPlusZ.material.color = colourPlusZ;
-        compassMinusX.material.color = colourMinusX;
-        compassMinusY.material.color = colourMinusY;
-        compassMinusZ.material.color = colourMinusZ;
+        Color.red,
+        Color.green,
+        Color.blue,
+        Color.cyan,
+        Color.magenta,
+        Color.yellow
+    };
+    public Renderer[] compassAxes;
+    public Transform compassTransform;
+
+    [Header("Complex GUI")]
+    public LineRenderer gridLineTemplate;
+    LineRenderer[] gridLines;
+    public static readonly Vector3[] axes = new Vector3[6]
+    {
+        Vector3.right,
+        Vector3.up,
+        Vector3.forward,
+        Vector3.left,
+        Vector3.down,
+        Vector3.back
+    };
+    public void ToggleComplexGUI()
+    {
+        ComplexGUIActive = !ComplexGUIActive;
     }
-    
+    public bool ComplexGUIActive
+    {
+        get
+        {
+            return complexGUIActive;
+        }
+        set
+        {
+            if (value == complexGUIActive)
+            {
+                return;
+            }
+
+            complexGUIActive = value;
+
+            for (int i = 0; i < axes.Length; i++)
+            {
+                gridLines[i].gameObject.SetActive(complexGUIActive);
+            }
+        }
+    }
+    bool complexGUIActive;
+
+    private void OnValidate()
+    {
+        if (axisColours.Length != axes.Length)
+        {
+            System.Array.Resize(ref axisColours, axes.Length);
+        }
+        if (compassAxes.Length != axes.Length)
+        {
+            System.Array.Resize(ref compassAxes, axes.Length);
+        }
+    }
+
+    private void Awake()
+    {
+        gridLines = new LineRenderer[axes.Length];
+        for (int i = 0; i < axes.Length; i++)
+        {
+            // Set name and colour of axis
+            gridLines[i] = (i == 0) ? gridLineTemplate : Instantiate(gridLineTemplate, transform);
+            gridLines[i].name = "Grid line: " + axes[i];
+            gridLines[i].colorGradient = new Gradient()
+            {
+                colorKeys = new GradientColorKey[]
+                {
+                    new GradientColorKey(axisColours[i], 0)
+                },
+                alphaKeys = new GradientAlphaKey[]
+                {
+                    new GradientAlphaKey(1, 0)
+                }
+            };
+
+            // Set positions and length
+            gridLines[i].SetPosition(0, axes[i] * SolarSystem.Current.minSafeDistanceFromSun);
+            gridLines[i].SetPosition(1, axes[i] * SolarSystem.Current.radius);
+
+            // Prematurely disable
+            gridLines[i].gameObject.SetActive(false);
+
+            // Set compass colours
+            compassAxes[i].material.color = axisColours[i];
+        }
+    }
     void LateUpdate()
     {
         // Identify relative rotation from camera orientation to point at compass position
@@ -42,4 +108,8 @@ public class SpaceshipNavigationGUI : MonoBehaviour
         Camera c = controller.viewCamera;
         compassTransform.rotation = Quaternion.FromToRotation(c.transform.forward, compassTransform.position - c.transform.position);
     }
+
+
+
+    
 }
