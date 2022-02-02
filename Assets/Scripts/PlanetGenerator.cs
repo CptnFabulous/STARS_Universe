2df3ebcc4bc8a440734e328afa20ee4a59a2e859
Marcus Mouyat <marcus@mouyat.com>
2022-02-02 19:20:51 +1100

    Planet generator from online database
    I have implemented the beginnings of the system for accessing user-generated planets. When the player loads into the spaceship level, instead of randomly generating planets, the game will access a Firebase database to obtain a list of user-generated planets. These planets will then be generated using the data contained in the database. I am very pleased with the core integration, but there are still many features to be implemented.
    For example, a large amount of planets in the database will cause hardware strain if too many exist in the level at once, so a mechanic will need to exist where planets are sorted into groups (e.g. solar systems), so the player can choose to travel to (i.e. load in) a specific set of planets at a time.
    Additionally, although the code still exists to spawn random planets as a default, there is currently no proper failsafe for if the game cannot establish a proper connection to the online database. I will need to implement a reliable and user-friendly way to inform the player if such an error occurs, so they can either travel to the placeholder planets or take an alternative action.
    Finally, obtaining the data and generating each random planet takes time, which will add up in a system dense with user-generated planets. In order to account for this and prevent the user from launching into an empty environment with planets appearing all around them, I will need to implement a loading screen.

---
+++ Assets/Scripts/PlanetGenerator.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Firestore;
+//using Firebase.Extensions.TaskExtension; // for ContinueWithOnMainThread
+using Firebase.Extensions;
+using UnityEngine.Networking;
+
+public class PlanetGenerator : MonoBehaviour
+{
+    [Header("Paths to database")]
+    public string instanceName = "STARS Universe";
+    public string databaseCollectionPath = "/starsData";
+
+    [Header("Planet attributes")]
+    public Mesh[] shapes;
+    public Material[] materialTypes;
+    public string planetTag = "Planet";
+
+    public Dictionary<string, Color> stringToColour;
+
+    [Header("Positioning")]
+    public float minDistanceFromCentre = 50;
+    public float maxDistanceFromCentre = 950;
+    public float minScale = 50;
+    public float maxScale = 200;
+
+    [Header("If random")]
+    public int randomPlanetCount = 100;
+    public bool forceRandomPlanetGeneration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stringToColour = new Dictionary<string, Color>
+        {
+            {"red", Color.red },
+            {"blue", Color.blue },
+            {"green", Color.green },
+            {"yellow", Color.yellow },
+            {"orange", new Color(1, 0.5f, 0) },
+            {"purple", new Color(0.5f, 0, 1) },
+            {"brown", new Color(0.5f, 0.25f, 0) },
+            {"pink", new Color(1f, 0.5f, 1f) },
+            {"cyan", Color.cyan },
+            {"magenta", Color.magenta },
+            {"white", Color.white },
+            {"black", Color.black },
+            {"grey", Color.grey },
+        };
+
+        StartCoroutine(LoadPlanetsFromDatabase());
+    }
+
+
+
+    IEnumerator LoadPlanetsFromDatabase()
+    {
+        FirebaseApp app = FirebaseApp.DefaultInstance;//GetInstance(instanceName);
+        FirebaseFirestore database = FirebaseFirestore.GetInstance(app);
+        CollectionReference collection = database.Collection(databaseCollectionPath);
+
+        System.Threading.Tasks.Task<QuerySnapshot> snapshotObtainingTask = collection.GetSnapshotAsync();
+        yield return new WaitUntil(() =>
+        {
+            return snapshotObtainingTask.IsCompleted || snapshotObtainingTask.IsFaulted || snapshotObtainingTask.IsCanceled;
+        });
+        QuerySnapshot snapshot = snapshot = snapshotObtainingTask.Result;
+
+        if (snapshot != null && forceRandomPlanetGeneration == false)
+        {
+            StartCoroutine(LoadPlanetsFromSnapshot(snapshot)); // Generate new planets from snapshot
+        }
+        else
+        {
+             LoadRandomPlanets(); // Generate random planets
+        }
+    }
+
+
+
+
+
+    IEnumerator LoadPlanetsFromSnapshot(QuerySnapshot snapshot)
+    {
+        //Debug.Log("New snapshot (length: " + snapshot.Count + ")");
+        for (int i = 0; i < snapshot.Count; i++)
+        {
+            //Debug.Log(snapshot[i]);
+            
+
+
+            string name;
+            Vector3 position = RandomPosition();
+            Quaternion rotation = RandomRotation();
+            float size = Random.value;
+            int shapeIndex = 0; // Set up index finding later, when this data is stored on the database
+            int materialIndex = 0; // Set up index finding later, when this data is stored on the database
+            Color retrievedColour;
+            Texture2D texture;
+
+
+            string colourName = snapshot[i].GetValue<string>("colour");
+            //Debug.Log(colourName);
+            if (stringToColour.TryGetValue(colourName, out retrievedColour) == false)
+            {
+                retrievedColour = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+            }
+
+            string imageURL = snapshot[i].GetValue<string>("img");
+            UnityWebRequest getTexture = UnityWebRequestTexture.GetTexture(imageURL);
+            yield return getTexture.SendWebRequest();
+            texture = DownloadHandlerTexture.GetContent(getTexture);
+
+            string ownerName = snapshot[i].GetValue<string>("name");
+            name = ownerName;
+            if (ownerName.EndsWith("s", true, System.Globalization.CultureInfo.InvariantCulture))
+            {
+                name += "'";
+            }
+            else
+            {
+                name += "'s";
+            }
+            name += " planet";
+
+            GeneratePlanet(name, position, rotation, size, shapeIndex, materialIndex, retrievedColour, texture);
+        }
+    }
+    void LoadRandomPlanets()
+    {
+        Debug.Log("Obtaining snapshot failed");
+
+        for (int i = 0; i < randomPlanetCount; i++)
+        {
+            string name = "Random Planet #" + (i + 1);
+
+            Vector3 position = Random.insideUnitSphere.normalized * Random.Range(minDistanceFromCentre, maxDistanceFromCentre);
+            Quaternion rotation = Quaternion.Euler(Random.Range(0f, 360), Random.Range(0f, 360), Random.Range(0f, 360));
+            float scaleValue = Random.value;
+
+            int shapeIndex = Random.Range(0, shapes.Length - 1);
+            int materialIndex = Random.Range(0, materialTypes.Length - 1);
+            Color planetColour = new Color(Random.Range(0f, 1), Random.Range(0f, 1), Random.Range(0f, 1)/*, Random.Range(0, 255)*/);
+            GeneratePlanet(name, position, rotation, scaleValue, shapeIndex, materialIndex, planetColour, null);
+        }
+
+    }
+
+    Vector3 RandomPosition()
+    {
+        return Random.insideUnitSphere.normalized * Random.Range(minDistanceFromCentre, maxDistanceFromCentre);
+    }
+    Quaternion RandomRotation()
+    {
+        return Quaternion.Euler(Random.Range(0f, 360), Random.Range(0f, 360), Random.Range(0f, 360));
+    }
+
+
+    void GeneratePlanet(string name, Vector3 position, Quaternion rotation, float size, int shapeIndex, int materialIndex, Color planetColour, Texture2D texture)
+    {
+        GameObject newPlanet = new GameObject(name);
+        newPlanet.transform.SetParent(transform);
+        newPlanet.tag = planetTag;
+
+        newPlanet.transform.localPosition = position;
+        newPlanet.transform.localRotation = rotation;
+        float scaleValue = Mathf.Lerp(minScale, maxScale, size);
+        Vector3 planetScaleValues = new Vector3(scaleValue, scaleValue, scaleValue);
+        newPlanet.transform.localScale = planetScaleValues;
+
+        MeshFilter filter = newPlanet.AddComponent<MeshFilter>();
+        MeshCollider collider = newPlanet.AddComponent<MeshCollider>();
+        Mesh shape = shapes[shapeIndex];
+        filter.mesh = shape;
+        collider.sharedMesh = shape;
+        collider.convex = true;
+
+        MeshRenderer renderer = newPlanet.AddComponent<MeshRenderer>();
+        renderer.material = materialTypes[materialIndex];
+        renderer.material.color = planetColour;
+        renderer.material.mainTexture = texture;
+    }
+}
